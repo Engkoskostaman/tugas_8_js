13b3b80911e56350cf7ba4f627ca78bba2c20393
Engkos <engkoskostaman97@gmail.com>
2019-08-01 02:07:50 +0700

    tugas 8 js
    tugas inin kami kumpulkan untuk niomic

---
+++ tugas8.js/index.js
@@ -0,0 +1,15 @@
+function panggilobjects(){
+  var laptop ={
+    type:'asus',
+    harga:5000000,
+    warna:'biru',
+
+  }
+  laptop.harga=4000000
+  laptop.warna='putih'
+  laptop.type='lenovo'
+  console.log(laptop)
+  console.log(laptop.harga)
+
+}
+panggilobjects()
